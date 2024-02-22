23fd7523fd1df2153701a138b3489c21a4a1b7b1
Stevens <stevensho@stevensho.com>
2024-02-21 22:38:09 -0500

    dependency stuff

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -3,7 +3,6 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
 import { Loader2 } from "lucide-react";
 
-
 const Page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -17,22 +16,19 @@ const Page = () => {
     },
   });
 
-
   if (error?.data?.code === "UNAUTHORIZED") {
     router.push("/api/auth/login");
   }
 
   return (
-    <div className='w-full mt-24 flex justify-center'>
-      <div className='flex flex-col items-center gap-2'>
-        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
-        <h3 className='font-semibold text-xl'>
-          Checking your account...
-        </h3>
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Checking your account...</h3>
         <p>You will be redirected automatically.</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Page;
