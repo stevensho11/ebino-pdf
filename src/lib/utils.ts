fa8620662592bc8c4b61d21c9626528aeb68a07b
Stevens Ho <stevensho@stevensho.com>
2024-02-23 03:45:30 -0500

    absolute url fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
   if (typeof window !== "undefined") return path;
   if (process.env.CUSTOM_DOMAIN_URL)
-    return `https://${process.env.CUSTOM_DOMAIN_URL}${path}`;
+    return `${process.env.CUSTOM_DOMAIN_URL}${path}`;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
